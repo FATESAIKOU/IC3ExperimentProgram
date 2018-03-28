39369ac3cd49b23b32bed3127ca923f3506613af
FATESAIKOU <qzsecftbhhhh@gmail.com>
2018-03-28 14:32:18 +0800

    Improve Load Right sequence making it affordable

--- src/LoadRights.py
+++ src/LoadRights.py
@@ -7,8 +7,10 @@ This program is for rights loading.
 @email  : qzsecftbhhhh@gmail.com
 @date   : 03/18/2018
 @argv[1]: right file
-@argv[2]: server address
-@argv[3]: server port
+@argv[2]: load size
+@argv[3]: chunk size
+@argv[3]: server address
+@argv[4]: server port
 """
 
 import asyncore
@@ -16,20 +18,41 @@ import logging
 import sys
 import json
 
-from IoTNetworkAgent import createClients
+from IoTNetworkAgent import IoTClient
 
 
 RIGHT_FILE  = sys.argv[1]
-SERVER_ADDR = sys.argv[2]
-SERVER_PORT = int(sys.argv[3])
+LOAD_SIZE   = int(sys.argv[2])
+CHUNK_SIZE  = int(sys.argv[3])
+SERVER_ADDR = sys.argv[4]
+SERVER_PORT = int(sys.argv[5])
 
 
-def checkRightsLen(right_file_path):
+def getRightsChunks(right_file_path, load_size, chunk_size):
     src = open(right_file_path, 'r')
     rights = json.loads(src.read())
     src.close()
-    
-    return len(rights)
+
+    if load_size > rights:
+        raise IndexError('Load_size is bigger than the rights_file\'s size.')
+
+    for i in range(0, load_size, chunk_size):
+        yield rights[i:min(i + chunk_size, load_size)]
+
+
+def createLoadRightClients(server_addr_pair, rights):
+    clients = [
+        IoTClient(
+            server_addr_pair,
+            json.dumps({
+                'cmd': 'get_access',
+                'content': lease
+            })
+        )
+        for lease in rights
+    ]
+
+    return clients
 
 
 if __name__ == '__main__':
@@ -37,5 +60,6 @@ if __name__ == '__main__':
                         format='%(name)s %(message)s')
 
 
-    clients = createClients((SERVER_ADDR, SERVER_PORT), 'get_access', 0, checkRightsLen(RIGHT_FILE), RIGHT_FILE)
-    asyncore.loop()
+    for rights_chunk in getRightsChunks(RIGHT_FILE, LOAD_SIZE, CHUNK_SIZE):
+        clients = createLoadRightClients((SERVER_ADDR, SERVER_PORT), rights_chunk)
+        asyncore.loop()
