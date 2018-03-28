d7a0fbe3594f1a0df4c916fdcdfad2bb3c476c5b
FATESAIKOU <qzsecftbhhhh@gmail.com>
2018-03-28 14:29:26 +0800

    Implement script to extract valid rights

--- tests/extract_right.py
+++ tests/extract_right.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
 """
-This program is a sequence to get experiment data for the
-IC3.
+This program is a sequence to extract right for the IC3.
 
 
 @author : FATESAIKOU
@@ -24,18 +23,8 @@ RIGHT_ROOT = sys.argv[1]
 RIGHT_PATH = sys.argv[2]
 
 
-if __name__ == '__main__':
-    # Extract valid filenames from RIGHT_ROOT
-    filename_list = []
-    for path, subdirs, files in os.walk(RIGHT_ROOT):
-        for filename in files:
-            if 'get_right' in filename:
-                f = os.path.join(path, filename)
-                filename_list.append(f)
-
-
-
-    # Read valid reight
+def readRight(filename_list, right_path):
+    # Read valid right
     tmp_right_list = []
     for filename in filename_list:
         src = open(filename, 'r')
@@ -48,31 +37,58 @@ if __name__ == '__main__':
             })
 
             tmp_right_list.append(r)
+    
+    tmp_right_list_set = set([json.dumps(t) for t in tmp_right_list])
 
 
     # Load old right file
-    src = open(RIGHT_PATH, 'r')
+    src = open(right_path, 'r')
     right_list = json.loads(src.read())
     src.close()
+    right_list_set = set([json.dumps(t) for t in right_list])
+
+    new_right_list_set = tmp_right_list_set - right_list_set
+    new_right_list = [json.loads(n) for n in new_right_list_set]
+
+    return [new_right_list, right_list]
+
+
+if __name__ == '__main__':
+    print "Rights Root:", RIGHT_ROOT
+    print "Rights Path:", RIGHT_PATH
+
+    # Extract valid filenames from RIGHT_ROOT
+    filename_list = []
+    for path, subdirs, files in os.walk(RIGHT_ROOT):
+        for filename in files:
+            if 'get_right' in filename:
+                f = os.path.join(path, filename)
+                filename_list.append(f)
+
+
+    # Read right
+    new_right_list, right_list = readRight(filename_list, RIGHT_PATH)
 
 
     # Test Transaction
     print 'Start testing transaction'
 
-    test_len = len(tmp_right_list)
+    test_len = len(new_right_list)
     print 'test len', test_len
 
-    cnt = 1
+    cnt = 0
+    r_cnt = 0
     api = Iota('http://localhost:14265')
-    for tmp_right in tmp_right_list:
-        res = api.find_transactions(tags=[tmp_right['tag']])
-        print 'Test Progress: ', cnt, '/', test_len
+    for new_right in new_right_list:
+        res = api.find_transactions(tags=[new_right['tag']])
+        print 'Test Progress: ', r_cnt, '/', cnt, '/', test_len
         cnt += 1
 
         if len(res['hashes']) != 1:
             continue
 
-        right_list.append(tmp_right)
+        r_cnt += 1
+        right_list.append(new_right)
 
 
     # Restore
